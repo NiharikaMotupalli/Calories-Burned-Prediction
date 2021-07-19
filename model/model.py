fc9461bd67f377eb0a227ee5e7fcaadf09c7f5a8
NiharikaMotupalli <niharikamotupalli99@gmail.com>
2021-07-19 11:10:23 +0530

    Updated

--- model/model.py
+++ model/model.py
@@ -1,15 +1,6 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import classification_report
-from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import BernoulliNB
 from imblearn.over_sampling import SMOTE
 import pickle
 import warnings 
